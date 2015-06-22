9b9300bd44533943d590607e5bff672f527544d8
vipc2010 <563309439@qq.com>
2015-06-22 13:44:55 +0800

    modified

--- yingjiesheng/pipelines.py
+++ yingjiesheng/pipelines.py
@@ -16,34 +16,34 @@ import MySQLdb.cursors
 import sys
 
 if sys.getdefaultencoding() != 'utf-8':
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
 
-
+countadd=0
 class YingjieshengPipeline(object):
 
-    def __init__(self):
-	self.dbpool = adbapi.ConnectionPool('MySQLdb',db='customer',user = 'root',passwd = 'vipc2010',cursorclass = MySQLdb.cursors.DictCursor,charset = 'utf8',use_unicode = False)
+	def __init__(self):
+		self.dbpool = adbapi.ConnectionPool('MySQLdb',db='customer',user = 'root',passwd = 'vipc2010',cursorclass = MySQLdb.cursors.DictCursor,charset = 'utf8',use_unicode = False)
 
-    def process_item(self, item, spider):
+	def process_item(self, item, spider):
 
-	query = self.dbpool.runInteraction(self._conditional_insert,item)
+		query = self.dbpool.runInteraction(self._conditional_insert,item)
 
-	query.addErrback(self.handle_error)
-        return item
+		query.addErrback(self.handle_error)
+		return item
 
-    def _conditional_insert(self,tx,item):
+	def _conditional_insert(self,tx,item):
 	
-	print '\n\n---------------------\n',item['company_name'],'\n------------\n\n'
+#	print '\n\n---------------------\n',item['company_name'],'\n------------\n\n'
 
-	tx.execute("select * from yingjiesheng_jiangsu where company_name = %s", (item['company_name'],))
-	result = tx.fetchone()
-	if result or item['company_name']=='':
-	    log.msg("\n\n\n\n\n\nItem already stored in db: %s" % item, level=log.DEBUG)
-	else:
-	    tx.execute(\
-		"insert into yingjiesheng_jiangsu (company_name,company_tel,company_email,company_link) values (%s, %s, %s,%s)",(item['company_name'], item['company_tel'], item['company_email'], item['company_link']))
-	    log.msg("\n\n\n\n\n\nItem stored in db: %s" % item, level=log.DEBUG)
+		tx.execute("select * from yingjiesheng_jiangsu where company_name = %s", (item['company_name'],))
+		result = tx.fetchone()
+		if result or item['company_name']=='':
+			log.msg("\n\n\n\n\n\nItem already stored in db: %s" % item, level=log.DEBUG)
+		else:
+			tx.execute(\
+			"insert into yingjiesheng_jiangsu (company_name,company_tel,company_email,company_link) values (%s, %s, %s,%s)",(item['company_name'], item['company_tel'], item['company_email'], item['company_link']))
+			log.msg("\n\n\n\n\n\nItem stored in db: %s" % item, level=log.DEBUG)
 
-    def handle_error(self,e):
-	log.err(e)
+	def handle_error(self,e):
+		log.err(e)
--- yingjiesheng/spiders/js.py
+++ yingjiesheng/spiders/js.py
@@ -4,62 +4,73 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
 from scrapy import log
-
+import time
 from yingjiesheng.items import YingjieshengItem
 
-class JsSpider(CrawlSpider):
-    name = "js"
-    allowed_domains = ["yingjiesheng.com"]
-    download_delay = 10
-    start_urls = ['http://www.yingjiesheng.com/jiangsujob/']
+today1 = time.strftime('%Y-%m-%d',time.localtime())
+today2 = time.strftime('%Y年%m月%d日',time.localtime())
 
-    rules = (
+class JsSpider(CrawlSpider):
+	name = "js"
+	allowed_domains = ["yingjiesheng.com"]
+	download_delay = 10
+	start_urls = ['http://www.yingjiesheng.com/jiangsujob/']
+
+	rules = (
 	Rule(SgmlLinkExtractor(allow=(r'http://www.yingjiesheng.com/jiangsujob/list_\d.html'))),
 	Rule(SgmlLinkExtractor(allow=(r'http://www.yingjiesheng.com/nanjing/'))),
 	Rule(SgmlLinkExtractor(allow=(r'http://www.yingjiesheng.com/wuxijob/[\w]*'))),
 	Rule(SgmlLinkExtractor(allow=(r'http://www.yingjiesheng.com/job-[\d-]+.html')),callback="parse_item"),
 	Rule(SgmlLinkExtractor(allow=(r'http://my.yingjiesheng.com/job_[\d]+.html')),callback="parse_item2"),
 )
-    def parse_item2(self,response):
+	def parse_item2(self,response):
 
-	item = YingjieshengItem()
-	company_name = response.xpath('//h1/a/text()').extract()
-	company_link = str(response.url)
-	company_tel = response.xpath('//div[@class="main"]').re(u'0[25][125][\d]?[)-]?[\u2014]?[\uff0d\u0020\u2014\uff09]?[0-9]{7,8}|1[358]\d-?\d{3,4}-?\d{4,5}')
-	company_email = response.xpath('//div[@class="main"]').re('[A-Za-z0-9\.-]+@(?:[A-Za-z0-9]+\.)+[A-Za-z]+')
+		itoday=response.xpath('//div[@class="job_list"]/ul/li/span/text()').re(today2.decode('utf-8'))
+		if itoday == []:
+			pass
+		else:
+			item = YingjieshengItem()
+			company_name = response.xpath('//h1/a/text()').extract()
+			company_link = str(response.url)
+			company_tel = response.xpath('//div[@class="main"]').re(u'0[25][125][\d]?[)-]?[\u2014]?[\uff0d\u0020\u2014\uff09]?[0-9]{7,8}|1[358]\d-?\d{3,4}-?\d{4,5}')
+			company_email = response.xpath('//div[@class="main"]').re('[A-Za-z0-9\.-]+@(?:[A-Za-z0-9]+\.)+[A-Za-z]+')
 	
-	item['company_name']=''.join(company_name).encode('utf-8')
-	item['company_link']=company_link.encode('utf-8')
-	item['company_tel']=','.join(sorted(set(company_tel))[:5]).encode('utf-8')
-	item['company_email']=','.join(sorted(set(company_email))[:5]).encode('utf-8')
+			item['company_name']=''.join(company_name).encode('utf-8')
+			item['company_link']=company_link.encode('utf-8')
+			item['company_tel']=','.join(sorted(set(company_tel))[:5]).encode('utf-8')
+			item['company_email']=','.join(sorted(set(company_email))[:5]).encode('utf-8')
 
-	if item['company_email'] and item['company_name']:
-	    return item
-	else:
-	    item['company_name']=''
-	    item['company_link']=''
-	    item['company_tel']=''
-	    item['company_email']=''
-	    return item
+			if item['company_email'] and item['company_name']:
+				return item
+			else:
+				item['company_name']=''
+				item['company_link']=''
+				item['company_tel']=''
+				item['company_email']=''
+				return item
 
-    def parse_item(self, response):
+	def parse_item(self, response):
 
-	item = YingjieshengItem()
+		itoday=response.xpath('//div[@class="info clearfix"]/ol/li/u/text()').re(today1.decode('utf-8'))
+		if itoday == []:
+			pass
+		else:
+			item = YingjieshengItem()
 
-	company_name = response.xpath('//div[@class="mleft"]/h1/text()').extract()
-	company_link = str(response.url)
-	company_tel = response.xpath('//div[@class="mleft"]').re(u'0[25][125][\d]?[)-]?[\u2014]?[\uff0d\u0020\u2014\uff09]?[0-9]{7,8}|1[358]\d-?\d{3,4}-?\d{4,5}')
-	company_email = response.xpath('//div[@class="mleft"]').re('[A-Za-z0-9\.-]+@(?:[A-Za-z0-9]+\.)+[A-Za-z]+')
-	item['company_name']=''.join(company_name).encode('utf-8')
-	item['company_link']=company_link.encode('utf-8')
-	item['company_tel']=','.join(sorted(set(company_tel))[:5]).encode('utf-8')
-	item['company_email']=','.join(sorted(set(company_email))[:5]).encode('utf-8')
+			company_name = response.xpath('//div[@class="mleft"]/h1/text()').extract()
+			company_link = str(response.url)
+			company_tel = response.xpath('//div[@class="mleft"]').re(u'0[25][125][\d]?[)-]?[\u2014]?[\uff0d\u0020\u2014\uff09]?[0-9]{7,8}|1[358]\d-?\d{3,4}-?\d{4,5}')
+			company_email = response.xpath('//div[@class="mleft"]').re('[A-Za-z0-9\.-]+@(?:[A-Za-z0-9]+\.)+[A-Za-z]+')
+			item['company_name']=''.join(company_name).encode('utf-8')
+			item['company_link']=company_link.encode('utf-8')
+			item['company_tel']=','.join(sorted(set(company_tel))[:5]).encode('utf-8')
+			item['company_email']=','.join(sorted(set(company_email))[:5]).encode('utf-8')
 
-        if item['company_email'] and item['company_name']:
-            return item
-        else:
-            item['company_name']=''
-            item['company_link']=''
-            item['company_tel']=''
-            item['company_email']=''
-            return item
+			if item['company_email'] and item['company_name']:
+				return item
+			else:
+				item['company_name']=''
+				item['company_link']=''
+				item['company_tel']=''
+				item['company_email']=''
+				return item
